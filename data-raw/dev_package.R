2091452035b739fbe35b5ac6c61425ebb50caeaf
blaserlab <bradley.blaser@osumc.edu>
2022-03-06 23:16:23 -0500

    Initial commit

---
+++ data-raw/dev_package.R
@@ -0,0 +1,8 @@
+# package setup
+library(usethis)
+create_package("/workspace/workspace_pipelines/blaseRtemplates")
+use_mit_license("Brad Blaser")
+use_readme_md()
+use_news_md()
+use_git()
+use_github(private = TRUE)
