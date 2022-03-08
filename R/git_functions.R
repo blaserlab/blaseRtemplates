45a3f170bfd01ba557791aaf0c5af8ee82984945
blaserlab <bradley.blaser@osumc.edu>
2022-03-08 10:49:59 -0500

    corrected error

--- R/git_functions.R
+++ R/git_functions.R
@@ -44,7 +44,6 @@ git_easy_branch <- function(branch) {
     gert::git_branch_create(branch)
   }
 }
-git_easy_branch("new")
 
 #' @title Update a Working Git Branch
 #' @description This function updates a git branch via rebase from a default upstream branch (usually "main").  You can explicitly provide the names of your working branch and the default upstream branch.  If not provided, the function will use the current branch as your working branch and will automatically identify the default upstream branch.  Internally this calls a system file that must be installed using blaseRtemplates::setup_git_collab().  The upstream branch also needs to be connected to a remote (e.g. github).
