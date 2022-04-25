b40d9b0f8e2729d5b4c5878940f2830f7dae8da8
blaserlab <bradley.blaser@osumc.edu>
2022-04-25 11:49:08 -0400

    edited imports

--- data-raw/dev_package.R
+++ data-raw/dev_package.R
@@ -3,6 +3,7 @@ devtools::document()
 
 # add, commit, push
 gert::git_add("*")
-gert::git_commit("version 0.0.0.9088")
+gert::git_commit("version 0.0.0.9089")
 gert::git_push()
 
+pak::pak("blaserlab/blaseRtemplates")
