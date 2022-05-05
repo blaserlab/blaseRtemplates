b6601d1ed8467ef3ab7b79b57067ba6e4648e811
blaserlab <bradley.blaser@osumc.edu>
2022-05-05 08:16:52 -0400

    version 0.0.0.9095

--- data-raw/dev_package.R
+++ data-raw/dev_package.R
@@ -1,13 +1,12 @@
-# renv::init()
+# renv::activate()
+# renv::init(project = "/workspace/brad_workspace/blaseRtemplates", bioconductor = TRUE)
 
 # document
 devtools::document()
 
 # add, commit, push
 gert::git_add("*")
-gert::git_commit("version 0.0.0.9094")
+gert::git_commit("version 0.0.0.9095")
 gert::git_push()
 
-pak::pkg_install("blaseRtemplates", lib = "/home/OSUMC.EDU/blas02/.cache/R/renv/library/blaseRtemplates-f5bc625c/R-4.2/x86_64-pc-linux-gnu")
-pak::pkg_install("blaseRtemplates", lib = "/opt/R/4.2.0/lib/R/library")
 
